7549ced230d239fcbaa72a4fcb65e5c000805097
Zijian Hu <zijianhu@usc.edu>
2018-01-09 05:08:54 -0800

    update lab

--- lab1/lab1.py
+++ lab1/lab1.py
@@ -15,11 +15,55 @@ class Run:
         self.time = factory.create_time_helper()
 
     def run(self):
+        def move(
+                right_wheel_velocity_in_mm_per_sec: float = 100.0,
+                left_wheel_velocity_in_mm_per_sec: float = 100.0,
+                num_seconds: float = 10.0
+        ) -> None:
+            self.create.drive_direct(right_wheel_velocity_in_mm_per_sec,
+                                     left_wheel_velocity_in_mm_per_sec)
+            self.time.sleep(num_seconds)
+
+        def wait(num_seconds: float = 10.0) -> None:
+            move(0, 0, num_seconds)
+
+        def move_forward(velocity_in_mm_per_sec: float = 100.0, num_seconds: float = 10.0) -> None:
+            move(velocity_in_mm_per_sec, velocity_in_mm_per_sec, num_seconds)
+
+        def move_backward(velocity_in_mm_per_sec: float = 100.0, num_seconds: float = 10.0) -> None:
+            move_forward(-velocity_in_mm_per_sec, num_seconds)
+
         self.create.start()
         self.create.safe()
 
-        self.create.drive_direct(100, 100)
-        self.time.sleep(10)
-        self.create.drive_direct(0, 0)
+        # move forward
+        move_forward(100, 5)
+
+        # left turn (in place)
+        move(100, -100, 2)
+
+        # wait
+        wait(2)
+
+        # right turn (in place)
+        move(-100, 100, 2)
+
+        # wait
+        wait(2)
+
+        # move forward
+        move_forward(100, 2)
+
+        # move forward while turning
+        move(200, 100, 7.5)
+
+        # move forward
+        move_forward(100, 5)
+
+        # move backwards
+        move_backward(100, 5)
+
+        # stop
+        wait(3)
 
         self.create.stop()
