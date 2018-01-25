57d1678c5b26bb9cd69db77d559dcf42a2c33de1
Zijian Hu <zijianhu@usc.edu>
2018-01-24 17:54:10 -0800

    udpate

---
+++ lab2/lab2_5.2.py
@@ -0,0 +1,35 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from typing import List
+
+
+class Run:
+    def __init__(self, factory):
+        """Constructor.
+
+        Args:
+            factory (factory.FactoryCreate)
+        """
+        self.create = factory.create_create()
+        self.time = factory.create_time_helper()
+        self.sonar = factory.create_sonar()
+
+    def run(self):
+        data = np.zeros(100)
+        for i in range(0, 100):
+            data[i] = self.sonar.get_distance()
+            print(data[i])
+            self.time.sleep(0.1)
+
+
+def plotHist(data: List[float] = None, num_bins: int = 20) -> None:
+    if data is None:
+        data = np.random.normal(0, 0.5, 10000)
+
+    plt.hist(data, bins=num_bins, edgecolor='black', linewidth=1.0)
+    plt.show()
+
+
+def showStat(data: List[float]) -> None:
+    print("Mean: " + np.mean(data))
+    print("SD: " + np.std(data))
