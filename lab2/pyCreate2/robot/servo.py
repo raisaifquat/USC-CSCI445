b9aca095ae5e679cff5ed3b80b3e0d4840035ec9
Zijian Hu <zijianhu@usc.edu>
2018-01-24 19:54:12 -0800

    update

--- lab2/pyCreate2/robot/servo.py
+++ lab2/pyCreate2/robot/servo.py
@@ -14,23 +14,25 @@ class Servo:
         """
         self.pwm = Pwm(number)
         self.pwm.enable()
-        self.full_pulse = 2.25e-3
-        # self.interval = 20e-3
-        # self.period = self.full_pulse + self.interval
+        self.full_pulse = 20.0e-3
         self.period = self.full_pulse
         self.frequency = int(1 / self.period)
-
-    def __del__(self) -> None:
-        self.pwm.disable()
-
-    def go_to(self, angle: float) -> None:
         # 0.75–2.25 ms high pulse, 20 ms intervals
+        # self.pwm.set_frequency(self.frequency)
         self.pwm.set_frequency(self.frequency)
 
+    def __del__(self) -> None:
+        # self.pwm.disable()
+        pass
+
+    def go_to(self, angle: float) -> None:
         # clamp angle to [-90.0, 90.0]
         angle = -90.0 if angle < -90.0 else angle
         angle = 90.0 if angle > 90.0 else angle
 
         # percent = (angle + 90) / 180 * self.full_pulse / self.period
-        percent = (angle + 90) / 180
+        num = 1125 - 375
+        percent = (angle + 90) / 180 * num + 375
+        percent /= 100.0
+        print("angle: " + str(angle) + "\npercent: " + str(percent))
         self.pwm.set_duty_cycle(percent)
