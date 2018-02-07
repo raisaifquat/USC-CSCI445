72e4956ebfdf211969e7581c6d9d2286adb4c292
Zijian Hu <zijianhu@usc.edu>
2018-02-07 03:32:58 -0800

    update

--- lab4/lab4.py
+++ lab4/lab4.py
@@ -59,32 +59,43 @@ class Run:
             self.create.drive_direct(v_left_update, v_right_update)
             state = self.create.update()
             if state is not None:
-                print(state.__dict__)
+                # print(state.__dict__)
                 time_elapsed = curr_time - prev_time
                 self.odometry.update(state.rightEncoderCounts, state.leftEncoderCounts, time_elapsed)
                 # print("[x = %f, y = %f, angle = %f]" % (self.odometry.x,
                 #                                         self.odometry.y,
                 #                                         np.rad2deg(self.odometry.angle)))
-                print("time_elapsed: %f\nr_speed: %f, l_speed: %f" % (
-                    time_elapsed, self.odometry.r_speed, self.odometry.l_speed
-                ))
+                # print("time_elapsed: %f\nr_speed: %f, l_speed: %f" % (
+                #     time_elapsed, self.odometry.r_speed, self.odometry.l_speed
+                # ))
 
-                # v_right_update = self.p_controller.update(v_right_goal, self.odometry.r_speed, False)
-                # v_left_update = self.p_controller.update(v_left_goal, self.odometry.l_speed, True)
-                v_right_update = self.pd_controller.update(v_right_goal, self.odometry.r_speed, curr_time, False)
-                v_left_update = self.pd_controller.update(v_left_goal, self.odometry.l_speed, curr_time, True)
-                print("v_right_update: %f, v_left_update: %f\n" % (v_right_update, v_left_update))
+                v_right_update = self.p_controller.update(v_right_goal, self.odometry.r_speed, False)
+                v_left_update = self.p_controller.update(v_left_goal, self.odometry.l_speed, True)
+                # v_right_update = self.pd_controller.update(v_right_goal, self.odometry.r_speed, curr_time, False)
+                # v_left_update = self.pd_controller.update(v_left_goal, self.odometry.l_speed, curr_time, True)
+                # print("v_right_update: %f, v_left_update: %f\n" % (v_right_update, v_left_update))
 
                 time_arr = np.append(time_arr, curr_time)
-                r_speed_arr = np.append(v_right_update, self.odometry.r_speed)
-                l_speed_arr = np.append(v_right_update, self.odometry.l_speed)
+                r_speed_arr = np.append(r_speed_arr, self.odometry.r_speed)
+                l_speed_arr = np.append(l_speed_arr, self.odometry.l_speed)
 
             prev_time = curr_time
             curr_time = self.time.time()
 
-        plt.plot(time_arr, r_speed_arr)
+        # plt.plot(time_arr, r_speed_arr)
         # plt.plot(time_arr, l_speed_arr)
-        plt.xlabel('time')
-        plt.ylabel('right wheel speed')
-        plt.grid()
-        plt.show()
+        # plt.xlabel('time')
+        # plt.ylabel('right wheel speed')
+        # plt.grid()
+        # plt.show()
+        file = open('output.txt', 'w')
+        file.write("time_arr = np.array([")
+        for i in range(0, len(time_arr)):
+            file.write("%f, " % time_arr[i])
+        file.write("])\n")
+
+        file.write("r_speed_arr = np.array([")
+        for i in range(0, len(r_speed_arr)):
+            file.write("%f, " % time_arr[i])
+        file.write("])")
+        file.close()
