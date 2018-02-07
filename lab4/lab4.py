e800e4280506fd2914b99c06087bf0c7c2895ccb
Zijian Hu <zijianhu@usc.edu>
2018-02-07 01:17:16 -0800

    update

--- lab4/lab4.py
+++ lab4/lab4.py
@@ -1,6 +1,14 @@
 from pyCreate2 import create2
 from p_controller import PController
+from odometry import Odometry
 
+
+robotProperties = {
+    "diameter_left": 72,
+    "diameter_right": 72,
+    "wheel_base": 235,
+    "encoder_count": 508.8
+}
 
 class Run:
     def __init__(self, factory):
@@ -11,6 +19,12 @@ class Run:
         """
         self.create = factory.create_create()
         self.time = factory.create_time_helper()
+        self.odometry = Odometry(
+            robotProperties["diameter_left"],
+            robotProperties["diameter_right"],
+            robotProperties["wheel_base"],
+            robotProperties["encoder_count"]
+        )
         self.p_controller = PController()
 
     def run(self):
@@ -24,4 +38,4 @@ class Run:
             state = self.create.update()
             if state is not None:
                 print(state.__dict__)
-                self.print_odometry(state)
+                self.(state)
--- lab4/odometry.py
+++ lab4/odometry.py
@@ -21,6 +21,8 @@ class Odometry:
         self.y = 0.0
         self.prev_r_count = 0
         self.prev_l_count = 0
+        self.l_speed = 0.0
+        self.r_speed = 0.0
 
     def get_delta_r(self, r_encoder_count: int) -> float:
         return self.diameter_right * r_encoder_count / self.wheel_base * np.pi
@@ -46,7 +48,7 @@ class Odometry:
 
         return (delta_r - delta_l) / self.wheel_base
 
-    def update(self, r_encoder_count: int = 0, l_encoder_count: int = 0):
+    def update(self, r_encoder_count: int = 0, l_encoder_count: int = 0, time_elapsed: float = 1.0):
 
         delta_r = self.get_delta_r(r_encoder_count - self.prev_r_count)
         delta_l = self.get_delta_l(l_encoder_count - self.prev_l_count)
@@ -55,7 +57,10 @@ class Odometry:
 
         delta_theta = self.get_delta_theta(delta_r=delta_r, delta_l=delta_l)
         delta_d = self.get_delta_d(delta_r=delta_r, delta_l=delta_l)
-        
+
         self.x += delta_d * np.cos(self.angle)
         self.y += delta_d * np.sin(self.angle)
         self.angle += delta_theta
+
+        self.r_speed = delta_r / time_elapsed
+        self.l_speed = delta_l / time_elapsed
