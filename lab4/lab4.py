5e00432c02aeca0c967eb34a266f53b8fb901c97
Zijian Hu <zijianhu@usc.edu>
2018-02-07 04:06:47 -0800

    update

--- lab4/lab4.py
+++ lab4/lab4.py
@@ -30,7 +30,7 @@ class Run:
             robotProperties["encoder_count"]
         )
         self.p_controller = PController(k_p=1.0)
-        self.pd_controller = PDController(k_p=1.5, k_d=-0.005)
+        self.pd_controller = PDController(k_p=1.0, k_d=-0.002)
 
     def run(self):
 
@@ -55,7 +55,7 @@ class Run:
         start_time = self.time.time()
         curr_time = start_time
         prev_time = curr_time
-        while curr_time - start_time < 2:
+        while curr_time - start_time < 3:
             self.create.drive_direct(v_left_update, v_right_update)
             state = self.create.update()
             if state is not None:
