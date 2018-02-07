570f9a6b0d5360199c86ca6e19707852f60fd418
Zijian Hu <zijianhu@usc.edu>
2018-02-07 03:11:24 -0800

    update

--- lab4/p_controller.py
+++ lab4/p_controller.py
@@ -2,15 +2,18 @@ class PController:
     def __init__(self, k_p: float = 0.0, range_min: float = -500.0, range_max: float = 500.0):
         self.range_min = range_min
         self.range_max = range_max
-        self.error = 0.0
+        self.l_error = 0.0
+        self.r_error = 0.0
 
         self.k_p = k_p
         pass
 
-    def update(self, goal: float, current_val: float) -> float:
+    def update(self, goal: float, current_val: float, is_left: bool) -> float:
         error = goal - current_val
-
-        self.error = error
+        if is_left:
+            self.l_error = error
+        else:
+            self.r_error = error
 
         return clamp(current_val + error * self.k_p, self.range_min, self.range_max)
 
--- lab4/pd_controller.py
+++ lab4/pd_controller.py
@@ -8,22 +8,26 @@ class PDController:
         self.k_d = k_d
 
         self.current_time = 0.0
-        self.error = 0.0
+        self.l_error = 0.0
+        self.r_error = 0.0
         pass
 
-    def update(self, goal: float, current_val: float, current_time: float = 0.0) -> float:
+    def update(self, goal: float, current_val: float, current_time: float, is_left: bool) -> float:
         error = goal - current_val
         delta_time = current_time - self.current_time
-        delta_error = error - self.error
+        delta_error = error - (self.l_error if is_left else self.r_error)
 
         de_dt = 0.0 if delta_time == 0 else delta_error / delta_time
 
         # print("de/dt = %f" % de_dt)
 
-        self.error = error
+        if is_left:
+            self.l_error = error
+        else:
+            self.r_error = error
         self.current_time = current_time
 
-        return clamp(current_val + self.error * self.k_p + self.k_d * de_dt, self.range_min, self.range_max)
+        return clamp(current_val + error * self.k_p + self.k_d * de_dt, self.range_min, self.range_max)
 
 
 def clamp(value, range_min, range_max):
