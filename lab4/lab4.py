46979b2dde8a5ac3dd495df65a9ca28eb43f8a5a
Zijian Hu <zijianhu@usc.edu>
2018-02-07 01:40:06 -0800

    update

--- lab4/lab4.py
+++ lab4/lab4.py
@@ -1,7 +1,7 @@
 from pyCreate2 import create2
 from p_controller import PController
 from odometry import Odometry
-
+import numpy as np
 
 robotProperties = {
     "diameter_left": 72,
@@ -9,6 +9,7 @@ robotProperties = {
     "wheel_base": 235,
     "encoder_count": 508.8
 }
+
 
 class Run:
     def __init__(self, factory):
@@ -32,10 +33,28 @@ class Run:
         self.create.start()
         self.create.safe()
 
+        # request sensors
+        self.create.start_stream([
+            create2.Sensor.LeftEncoderCounts,
+            create2.Sensor.RightEncoderCounts,
+        ])
+
         start_time = self.time.time()
-        while self.time.time() - start_time < 10:
-            self.create.drive_direct(50, -50)
+        curr_time = start_time
+        prev_time = curr_time
+        while curr_time - start_time < 1:
+            self.create.drive_direct(100, 100)
             state = self.create.update()
             if state is not None:
                 print(state.__dict__)
-                self.(state)
+                time_elapsed = curr_time - prev_time
+                self.odometry.update(state.rightEncoderCounts, state.leftEncoderCounts, time_elapsed)
+                print("[x = %f, y = %f, angle = %f]" % (self.odometry.x,
+                                                        self.odometry.y,
+                                                        np.rad2deg(self.odometry.angle)))
+                print("time_elapsed: %f\nr_speed: %f, l_speed: %f\n" % (
+                    time_elapsed, self.odometry.r_speed, self.odometry.l_speed
+                ))
+
+            prev_time = curr_time
+            curr_time = self.time.time()
