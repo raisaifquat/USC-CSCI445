bf54db785b44a15a4c7043a26a92a96d1d5d3d3f
Zijian Hu <zijianhu@usc.edu>
2018-02-13 04:06:34 -0800

    update

--- assignment2/assignment2.py
+++ assignment2/assignment2.py
@@ -1,6 +1,7 @@
 from pyCreate2 import create2
 
-import math
+import numpy as np
+import matplotlib.pyplot as plt
 
 from odometry import Odometry
 
@@ -16,20 +17,30 @@ class Run:
         self.time = factory.create_time_helper()
         self.odometry = Odometry()
 
+        self.x_arr = np.array([])
+        self.y_arr = np.array([])
+        self.x_arr_truth = np.array([])
+        self.y_arr_truth = np.array([])
+        self.time_arr = np.array([])
+
     def sleep(self, time_in_sec):
         """Sleeps for the specified amount of time while keeping odometry up-to-date
         Args:
             time_in_sec (float): time to sleep in seconds
         """
         start = self.time.time()
-        while True:
+        t = start
+        while t - start < time_in_sec:
             state = self.create.update()
             if state is not None:
                 self.odometry.update(state.leftEncoderCounts, state.rightEncoderCounts)
-                print("[{},{},{}]".format(self.odometry.x, self.odometry.y, math.degrees(self.odometry.theta)))
+                # print("[{},{},{}]".format(self.odometry.x, self.odometry.y, np.rad2deg(self.odometry.theta)))
+                self.time_arr = np.append(self.time_arr, t)
+                self.x_arr = np.append(self.x_arr, self.odometry.x)
+                self.y_arr = np.append(self.y_arr, self.odometry.y)
+                self.x_arr_truth = np.append(self.x_arr_truth, self.create.sim_get_position()[0])
+                self.y_arr_truth = np.append(self.y_arr_truth, self.create.sim_get_position()[1])
             t = self.time.time()
-            if start + time_in_sec <= t:
-                break
 
     def run(self):
         self.create.start()
@@ -41,8 +52,8 @@ class Run:
             create2.Sensor.RightEncoderCounts,
         ])
 
-        speed = 100  # mm/s
-        time_90deg_turn = 1.9  # seconds
+        speed = 300  # mm/s
+        time_90deg_turn = 1.9 / speed * 100.0  # seconds
 
         # move forward for 1m
         self.create.drive_direct(speed, speed)
@@ -71,3 +82,14 @@ class Run:
         # move forward for 0.5m
         self.create.drive_direct(speed, speed)
         self.sleep(500 / speed)
+
+        # stop simulation
+        self.create.stop()
+
+        plt.plot(self.time_arr, self.x_arr, label='x coor measured')
+        plt.plot(self.time_arr, self.x_arr_truth, label='x coor truth')
+        plt.plot(self.time_arr, self.y_arr, label='y coor measured')
+        plt.plot(self.time_arr, self.y_arr_truth, label='y coor truth')
+        plt.legend()
+        plt.grid()
+        plt.show()
