499dae968d70457313bd8857f45b6c09b961ae0e
Zijian Hu <zijianhu@usc.edu>
2018-03-01 01:54:27 -0800

    update

--- lab6and7/lab7.py
+++ lab6and7/lab7.py
@@ -102,9 +102,9 @@ class Run:
             [0.0, 1.0],
             [0.0, 0.0]
         ])
-        dist_threshold = 0.1
+        dist_threshold = 0.05
         wall_threshold = 0.4
-        distance_offset = 0.2
+        distance_offset = 0.1
         for point in waypoints:
             goal_x = point[0]
             goal_y = point[1]
@@ -113,7 +113,7 @@ class Run:
 
             print("Going to @{%.4f, %.4f}" % (goal_x, goal_y))
             # dist_to_wall = self.sweep_sonar(10, sleep_time=0.05)
-            dist_to_wall = self.sonar.get_distance()
+            # dist_to_wall = self.sonar.get_distance()
             curr_angle = math.degrees(self.odometry.theta)
             prev_dist_to_goal = self.dist_to_goal(goal_x, goal_y)
 
@@ -131,10 +131,11 @@ class Run:
                 elif curr_state is State.wall_following:
                     # sonar_angle = math.degrees(self.odometry.theta)
                     print("current angle %f" % curr_angle)
+                    goal_dist_to_wall = wall_threshold
 
                     self.follow_wall(wall_threshold, base_speed=base_speed)
                     self.servo.go_to(-curr_angle)
-                    self.sleep(0.1)
+                    self.sleep(0.5)
 
                     # print("prev angle %f" % sonar_angle)
                     curr_angle = math.degrees(self.odometry.theta)
@@ -142,7 +143,8 @@ class Run:
                     # sonar_angle = math.degrees(self.odometry.theta)
 
                     dist_offset = self.dist_to_goal(goal_x, goal_y) - prev_dist_to_goal
-                    if dist_offset < 0 or dist_offset >= distance_offset:
+                    dist_to_wall = self.dist_to_wall()
+                    if dist_to_wall >= (goal_dist_to_wall * 1.1) or dist_offset >= distance_offset:
                         curr_state = State.update
 
                 elif curr_state is State.init_wall_following:
@@ -157,8 +159,8 @@ class Run:
                     if dist_to_wall is not None and dist_to_wall > wall_threshold:
                         print("\ngo to goal!")
                         curr_state = State.go_to_goal
-                        self.servo.go_to(0)
-                        self.sleep(0.1)
+                        # self.servo.go_to(0)
+                        # self.sleep(0.1)
                     else:
                         curr_state = State.init_wall_following
 
