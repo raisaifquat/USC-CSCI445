72756c61491449ed867adceecb0b2f9d2e0b9e51
Zijian Hu <zijianhu@usc.edu>
2018-03-07 22:13:56 -0800

    update

--- lab6and7/lab7_timeout.py
+++ lab6and7/lab7_timeout.py
@@ -23,9 +23,9 @@ class Run:
         self.servo = factory.create_servo()
         self.odometry = odometry.Odometry()
         # self.pidTheta = pd_controller2.PDController(500, 100, -200, 200, is_angle=True)
-        self.pidTheta = pid_controller.PIDController(300, 5, 50, [-10, 10], [-200, 200], is_angle=True)
-        self.pidDistance = pid_controller.PIDController(1000, 0, 50, [0, 0], [-200, 200], is_angle=False)
-        self.pidWallFollow = pid_controller.PIDController(1000, 0, 100, [-75, 75], [-200, 200], is_angle=False)
+        self.pidTheta = pid_controller.PIDController(300, 5, 50, [-10, 10], [-300, 300], is_angle=True)
+        self.pidDistance = pid_controller.PIDController(1000, 0, 50, [0, 0], [-300, 300], is_angle=False)
+        self.pidWallFollow = pid_controller.PIDController(1000, 0, 100, [-75, 75], [-300, 300], is_angle=False)
 
     def sleep(self, time_in_sec, is_get_dist: bool = False, interrupt=lambda x: False):
         result = math.inf
