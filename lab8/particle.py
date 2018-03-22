9eb4860e535a99532801d13ed52b32bdd0c2a941
Zijian Hu <zijianhu@usc.edu>
2018-03-21 21:04:47 -0700

    update

--- lab8/particle.py
+++ lab8/particle.py
@@ -23,16 +23,17 @@ class Particle:
         # if (distance < 0):
         #     raise ValueError('robot cannot move backwards')
 
-        self.theta = self.theta + turn + np.random.normal(0.0, self.sd_direction)
+        # self.theta = self.theta + turn + np.random.normal(0.0, self.sd_direction)
+        self.theta = turn + np.random.normal(0.0, self.sd_direction)
         self.theta %= 2 * np.pi
 
         dist = distance + np.random.normal(0.0, self.sd_distance)
         self.x = self.x + dist * np.cos(self.theta)
         self.y = self.y - dist * np.sin(self.theta)
 
-        def sense(self, sensor_reading):
-            # calcuate posterior probablity for this particle
-            p_robot_at_location = self.map.closest_distance((self.x, self.y), self.theta)
+    def sense(self, sensor_reading):
+        # calculate posterior probability for this particle
+        p_robot_at_location = self.map.closest_distance((self.x, self.y), self.theta)
 
-        p_z_given_robot_at_location = norm.pdf(value, loc=p_robot_at_location, scale=self.sd_sensor)
+        p_z_given_robot_at_location = norm.pdf(sensor_reading, loc=p_robot_at_location, scale=self.sd_sensor)
 
