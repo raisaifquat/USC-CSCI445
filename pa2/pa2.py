d22fec55a1a25fc061b13084e0d01b6e2fceb143
Zijian Hu <zijianhu@usc.edu>
2018-03-27 12:03:05 -0700

    update

--- pa2/pa2.py
+++ pa2/pa2.py
@@ -24,19 +24,35 @@ class Run:
         self.z = 0.0
 
     def go_to_angle(self, angle_lower_arm, angle_upper_arm):
-        theta1 = math.radians(angle_lower_arm)
-        theta2 = math.radians(angle_upper_arm)
+        # angle: < 0 to the right, > 0 to the left
+        theta1 = math.radians(90 - abs(angle_lower_arm))
+        theta2 = math.radians(abs(angle_upper_arm))
 
-        self.arm.go_to(self.lower_arm_joint_number, theta1)
-        self.arm.go_to(self.upper_arm_joint_number, theta2)
+        self.arm.go_to(self.lower_arm_joint_number, math.radians(angle_lower_arm))
+        self.arm.go_to(self.upper_arm_joint_number, math.radians(angle_upper_arm))
+
         self.x = self.L1 * math.cos(theta1) + self.L2 * math.cos(theta1 + theta2)
-        self.y = self.L1 * math.sin(theta1) + self.L2 * math.sin(theta1 + theta2)
+        self.z = self.L1 * math.sin(theta1) + self.L2 * math.sin(theta1 + theta2)
 
-        print("Go to {:.2f}, {:.2f} deg, FK: [{:.2f}, {:.2f}, {:.2f}]".format(angle_lower_arm, angle_upper_arm, self.x, self.y, self.z))
+        print("Go to {:.2f}, {:.2f} deg, FK: [{:.2f}, {:.2f}, {:.2f}]"
+              .format(angle_lower_arm, angle_upper_arm, self.x, self.y, self.z + 0.3105))
+
+    def go_to_position(self, x, y):
+        # angle: < 0 to the right, > 0 to the left
+        angle_lower_arm = 0.0
+        angle_upper_arm = 0.0
+
+        print("Go to [{:.2f}, {:.2f}], IK: [{:.2f}, {:.2f}]"
+              .format(x, y, angle_lower_arm, angle_upper_arm))
 
     def run(self):
-        # self.arm.go_to(3, 45)
-        self.go_to_angle(45, -90)
+        angle = -90
+        # self.arm.go_to(self.upper_arm_joint_number, math.radians(angle))
+        # self.go_to_angle(45, -90)
+        # self.time.sleep(10)
+        self.go_to_angle(90, -70)
         self.time.sleep(10)
-        self.go_to_angle(45, -90)
-        self.time.sleep(10)
+        # self.go_to_angle(20, -180)
+        # self.time.sleep(10)
+        # self.go_to_angle(45, -10)
+        # self.time.sleep(10)
